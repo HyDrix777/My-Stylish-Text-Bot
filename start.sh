cad3d0c9e5a90588217ff03b4d9e2c39ba877e23
HyDrix777 <94365103+HyDrix777@users.noreply.github.com>
2022-05-03 12:21:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/HyDrix777/My-Stylish-Text-Bot.git /Stylish-Text-Bot
-cd /Stylish-Text-Bot
+git clone https://github.com/HyDrix777/My-Stylish-Text-Bot.git /My-Stylish-Text-Bot
+cd /My-Stylish-Text-Bot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
